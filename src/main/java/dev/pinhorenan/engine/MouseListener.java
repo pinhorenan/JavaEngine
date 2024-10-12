e80d23aacc08874ca8c3b2b325b7698aa1e850d5
Renan Pinho <pinhorenan@icloud.com>
2024-10-11 22:40:24 -0300

    First Commit
    Signed-off-by: Renan Pinho <pinhorenan@icloud.com>

--- src/main/java/dev/pinhorenan/engine/MouseListener.java
+++ src/main/java/dev/pinhorenan/engine/MouseListener.java
@@ -1,4 +1,9 @@
 package dev.pinhorenan.engine;
 
 public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX, scrollY;
+    private double xPosition, getScrollY, lastY, lastX;
+    private boolean[] mouseButtonPressed = new boolean[3];
+    private boolean isDragging;
 }
